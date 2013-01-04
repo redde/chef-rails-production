db70b423e78227021300698972dabdfc2dce8e34
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-01-05 01:58:28 +0400

    added users recipe

---
+++ cookbooks/users/recipes/default.rb
@@ -0,0 +1,31 @@
+include_recipe "sudo"
+
+user node['user'] do
+  supports :manage_home => true
+  comment "Rails production user"
+  home "/home/#{node['user']}"
+  shell "/bin/bash"
+  password (0...32).map{ ('a'..'z').to_a[rand(26)] }.join
+  action :create
+  not_if("ls /home | grep #{node['user']}")
+end
+
+execute "generate ssh key for user" do
+  user "webmaster"
+  command "ssh-keygen -t rsa -q -f /home/#{node['user']}/.ssh/id_rsa -P \"\""
+  not_if { File.exists?("/home/#{node['user']}/.ssh/id_rsa") }
+end
+
+template "/home/#{node['user']}/.ssh/authorized_keys" do
+  user node['user']
+  owner node['user']
+  source "authorized_keys"
+  mode 0600
+end
+
+template "/home/#{node['user']}/.ssh/known_hosts" do
+  user node['user']
+  owner node['user']
+  source "known_hosts"
+  mode 0600
+end
