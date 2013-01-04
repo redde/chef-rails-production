413ba15bbde7a9507beb78adcc88cefb07b43e24
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-01-05 01:36:11 +0400

    added main recipe

---
+++ cookbooks/chef-rails-production/metadata.rb
@@ -0,0 +1,5 @@
+name "chef-rails-production"
+version "0.1.0"
+description "Chef recipe for ubuntu/debian server configuration for rails production stack"
+maintainer "Oleg Bovykin"
+maintainer_email "oleg.bovykin@gmail.com"
