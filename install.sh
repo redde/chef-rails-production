69934c10867afb3a48e4662d9209911590187ee3
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-01-05 19:23:21 +0400

    fix in install

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-echo "Installing Git"
-apt-get install git -y -q;
-echo "Cloning recipes"
+echo "=========== Installing Git"
+apt-get install git-core -y -q;
+echo "=========== Cloning recipes"
 cd /tmp && git clone git://github.com/redde/chef-rails-production.git chef-cookbooks;
 cd /tmp/chef-cookbooks;
-echo "Running wizard"
+echo "=========== Running wizard"
 ruby wizard.rb;
-echo "Installing chef gem"
+echo "=========== Installing chef gem"
 gem install chef --no-ri --no-rdoc;
 ln -s /var/lib/gems/1.8/bin/chef-solo /bin;
-echo "Starting provisioning"
+echo "=========== Starting provisioning"
 ./cheffy.sh;
