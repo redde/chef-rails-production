1db38b006eeea9932b01ffa274cfa88671048bff
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-01-05 17:35:29 +0400

    added monit and fixed hash syntax error

--- cookbooks/chef-rails-production/recipes/default.rb
+++ cookbooks/chef-rails-production/recipes/default.rb
@@ -34,3 +34,17 @@ SQL
 execute "mysql create db and user" do
   command "mysql -u root -h localhost -e '#{db_sql}' -p #{node['mysql']['server_root_password']}"
 end
+
+package "monit" do
+  action :install
+end
+
+template "/etc/monit/conf.d/#{node['app_name']}" do
+  user "root"
+  owner "root"
+  source "unicorn.monitrc.erb"
+end
+
+service "monit" do
+  action [:enable, :start]
+end
