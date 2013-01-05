fc5bcb3d8d26efa0f5f75e663eb1864c3596201a
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-01-05 19:21:28 +0400

    fixed echo in install sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-ECHO "Installing Git"
+echo "Installing Git"
 apt-get install git -y -q;
-ECHO "Cloning recipes"
+echo "Cloning recipes"
 cd /tmp && git clone git://github.com/redde/chef-rails-production.git chef-cookbooks;
 cd /tmp/chef-cookbooks;
-ECHO "Running wizard"
+echo "Running wizard"
 ruby wizard.rb;
-ECHO "Installing chef gem"
+echo "Installing chef gem"
 gem install chef --no-ri --no-rdoc;
 ln -s /var/lib/gems/1.8/bin/chef-solo /bin;
-ECHO "Starting provisioning"
+echo "Starting provisioning"
 ./cheffy.sh;
